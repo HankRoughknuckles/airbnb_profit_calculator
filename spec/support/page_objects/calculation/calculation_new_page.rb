551a7efbfe0d1b7d58b337bfd623f4679e038805
Thomas Morris <tmorris@covermymeds.com>
2017-11-04 13:27:25 -0400

    Make the skeleton for showing the total
    As of now, we'll plan to make the 'new' action call the 'create' action
    with a POST.  (inside of which we will scrape AirBnB (to be completed))
    and redirect to the new action.
    
    The 'new' template will display the outputs of the calculation if the
    query string parameters are present:
    params: {
      long_term_rent: 235, # the amount one can make through long term rent
      airbnb_rent: 235, # the amount one can make through airbnb
    }

--- spec/support/page_objects/calculation/calculation_new_page.rb
+++ spec/support/page_objects/calculation/calculation_new_page.rb
@@ -1,9 +1,14 @@
+require "addressable/uri"
+
 class CalculationNewPage
   include Capybara::DSL
   include Rails.application.routes.url_helpers
 
   PAGE_URL = '/'.freeze
   TITLE = 'AirBnB Profit Calculator'.freeze
+  LONG_TERM_RENT_DISPLAY = '.long-term-rent-display'.freeze
+  AIRBNB_RENT_DISPLAY = '.airbnb-rent-display'.freeze
+  CALCULATED_DISPLAY = '.total'.freeze
 
   def initialize
   end
@@ -12,7 +17,53 @@ class CalculationNewPage
     visit PAGE_URL
   end
 
+  def visit_page_with_params(params)
+    uri = Addressable::URI.new
+    uri.query_values = params
+    visit "#{PAGE_URL}?#{uri.query}"
+  end
+
   def has_proper_title?
     has_title? TITLE
+  end
+
+  #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  #%% form inputs
+  ##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  def has_edit_user_link?
+    has_css? @edit_user_link
+  end
+
+  def click_edit_user_link
+    find(@edit_user_link).click
+  end
+
+
+  #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  #%% param display outputs
+  ##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  def has_long_term_rent_display?
+    has_css?(LONG_TERM_RENT_DISPLAY)
+  end
+
+  def has_airbnb_rent_display?
+    has_css?(AIRBNB_RENT_DISPLAY)
+  end
+
+  def has_calculated_total?
+    has_css?(CALCULATED_DISPLAY)
+  end
+
+  def has_long_term_rent_display_of?(rent)
+    has_css?(LONG_TERM_RENT_DISPLAY, text: "Your long term rent: $#{rent}")
+  end
+
+  def has_airbnb_rent_display_of?(rent)
+    has_css?(AIRBNB_RENT_DISPLAY, text: "Your AirBnb rent potential: $#{rent}")
+  end
+
+  def has_calculated_total_of?(total)
+    has_css?(CALCULATED_DISPLAY,
+             text: "You could make #{total} profit by using AirBnb!")
   end
 end
