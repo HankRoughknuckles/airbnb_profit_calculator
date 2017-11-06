a6eccd5fe3f3d208f02445a47112d765f90fb8d4
Thomas Morris <tmorris@covermymeds.com>
2017-11-05 19:33:07 -0500

    Setup js for google autocomplete - will choose city only

--- app/assets/javascripts/calculation.js
+++ app/assets/javascripts/calculation.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
+
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+            (document.getElementById('address')),
+            {types: ['(cities)']});
+}
+
